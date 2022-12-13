3b9443129ad15aa13982c75b98ee3abb1abaf5a9
nicholasboari <nicholasboari@gmail.com>
2022-12-13 14:52:45 -0300

    Exception handling - delete

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -3,7 +3,10 @@ package com.educandoweb.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import com.educandoweb.course.services.exceptions.DatabaseException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.course.entities.User;
@@ -13,35 +16,41 @@ import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 @Service
 public class UserService {
 
-  @Autowired
-  private UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-  public List<User> findAll() {
-    return userRepository.findAll();
-  }
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
 
-  public User findById(Long id) {
-    Optional<User> obj = userRepository.findById(id);
-    return obj.orElseThrow(() -> new ResourceNotFoundException(id));
-  }
+    public User findById(Long id) {
+        Optional<User> obj = userRepository.findById(id);
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+    }
 
-  public User insert(User obj) {
-    return userRepository.save(obj);
-  }
+    public User insert(User obj) {
+        return userRepository.save(obj);
+    }
 
-  public void delete(Long id) {
-    userRepository.deleteById(id);
-  }
+    public void delete(Long id) {
+        try {
+            userRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+    }
 
-  public User update(Long id, User obj) {
-    User entity = userRepository.getReferenceById(id);
-    updateData(entity, obj);
-    return userRepository.save(entity);
-  }
+    public User update(Long id, User obj) {
+        User entity = userRepository.getReferenceById(id);
+        updateData(entity, obj);
+        return userRepository.save(entity);
+    }
 
-  private void updateData(User entity, User obj) {
-    entity.setName(obj.getName());
-    entity.setEmail(obj.getEmail());
-    entity.setPhone(obj.getPhone());
-  }
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
+    }
 }
